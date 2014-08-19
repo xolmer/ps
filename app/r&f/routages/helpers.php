df82d775d2415cdc967e3a78b8d21cc8e4af1de1
saarang <saarang@mint.com>
2014-08-20 03:04:54 +0800

    added a test route

--- app/r&f/routages/helpers.php
+++ app/r&f/routages/helpers.php
@@ -2,6 +2,6 @@
 Route::group(array('prefix' => 'helpers'), function() {
    Route::get('banknames',array('as' => 'banknames','uses'=>'HelpersController@getBankNames'));
    Route::get('noscript', function() { return 'You landed on this page because JavaScript is disabled in your browser';});
-   Route::get('test',funciton(){ return 'You are being tested';});
+   Route::get('test',function(){ return 'You are being tested';});
 
 });
