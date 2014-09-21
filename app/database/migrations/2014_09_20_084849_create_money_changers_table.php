15166628735744ec02f532b5eaaf55c644a5d70c
saarang <saarang@mint.com>
2014-09-21 18:25:56 +0800

    moneychangers migration updated to include 1 phone_numner only

--- app/database/migrations/2014_09_20_084849_create_money_changers_table.php
+++ app/database/migrations/2014_09_20_084849_create_money_changers_table.php
@@ -20,10 +20,9 @@ class CreateMoneyChangersTable extends Migration {
 			$table->string('director_name');
 			$table->string('director_last_name');
 			$table->string('address')->nullable();
-			$table->string('phone_number1')->nullable();
-			$table->string('phone_number2')->nullable();
+			$table->string('phone_number')->nullable();
 			$table->string('mobile_number')->nullable();
-			$table->string('fax')->nullable();
+			$table->string('fax_number')->nullable();
 			$table->string('email_address')->nullable();
 			$table->softDeletes();
 			$table->timestamps();
